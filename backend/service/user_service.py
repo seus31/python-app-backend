ab7668dc75452bb27c9bd9a4c687312a3405a040
seus31 <us.balut.big.dipper@gmail.com>
2024-07-26 18:32:28 +0900

    fix: userサービスの修正

--- backend/service/user_service.py
+++ backend/service/user_service.py
@@ -1,7 +1,17 @@
 from flask import make_response, jsonify
-from model.user import User, UserSchema
+from backend.model.user import User, UserSchema
+
 
 def get_user_logic():
+    users = User.get_user_list()
+    user_schema = UserSchema(many=True)
+    return make_response(jsonify({
+        'code': 200,
+        'users': user_schema.dump(users)
+    }))
+
+
+def post_user_logic():
     users = User.get_user_list()
     user_schema = UserSchema(many=True)
     return make_response(jsonify({
