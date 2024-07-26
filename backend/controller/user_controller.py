0d2029e693fd854ca85a2d76afdcf6c3be953092
seus31 <us.balut.big.dipper@gmail.com>
2024-07-26 18:32:18 +0900

    fix: userコントローラーの修正

--- backend/controller/user_controller.py
+++ backend/controller/user_controller.py
@@ -1,4 +1,10 @@
-from service.user_service import get_user_logic
+from backend.service.user_service import get_user_logic
+from backend.service.user_service import post_user_logic
+
 
 def get_user():
     return get_user_logic()
+
+
+def post_user():
+    return post_user_logic()
