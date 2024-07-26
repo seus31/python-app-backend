edfd109652461f7160875c651afcbbc9ba44ee38
seus31 <us.balut.big.dipper@gmail.com>
2024-07-26 18:32:08 +0900

    fix: userモデルの修正

--- backend/model/user.py
+++ backend/model/user.py
@@ -2,10 +2,13 @@ from db import db, ma
 from sqlalchemy.dialects.mysql import TIMESTAMP as Timestamp
 from sqlalchemy.sql.functions import current_timestamp
 
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     name = db.Column(db.String(225), nullable=False)
+    email = db.Column(db.String(225), nullable=False, unique=True)
+    password = db.Column(db.String(225), nullable=False)
     created_at = db.Column(Timestamp, server_default=current_timestamp(), nullable=False)
     updated_at = db.Column(Timestamp, server_default=current_timestamp(), nullable=False)
 
@@ -17,30 +20,30 @@ class User(db.Model):
         self.updated_at = updated_at
 
     def __repr__(self):
-         return '<User %r>' % self.name
+        return '<User %r>' % self.name
 
     def get_user_list():
         # SELECT * FROM users
         user_list = db.session.query(User).all()
-        if user_list == None:
+        if user_list is None:
             return []
         else:
             return user_list
 
     def get_user_by_id(id):
-        return db.session.query(User)\
-            .filter(User.id == id)\
+        return db.session.query(User) \
+            .filter(User.id == id) \
             .one()
 
     def create_user(user):
         record = User(
-            name = user['name'],
+            name=user['name'],
         )
-        # INSERT INTO users(name) VALUES(...)
         db.session.add(record)
         db.session.commit()
         return user
 
+
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-      model = User
+        model = User
