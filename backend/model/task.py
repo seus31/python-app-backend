9188b20a6acf2a79b1c3f9bcfd201d4c91565bee
seus31 <us.balut.big.dipper@gmail.com>
2024-08-08 00:39:58 +0900

    add: タスクモデルの作成

---
+++ backend/model/task.py
@@ -0,0 +1,34 @@
+from db import db, ma
+from sqlalchemy.dialects.mysql import TIMESTAMP as Timestamp
+from sqlalchemy.sql.functions import current_timestamp
+
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    task_name = db.Column(db.String(225), nullable=False)
+    status = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(Timestamp, server_default=current_timestamp(), nullable=False)
+    updated_at = db.Column(Timestamp, server_default=current_timestamp(), nullable=False)
+
+    # Contractor
+    def __init__(self, task_name, status):
+        self.task_name = task_name
+        self.status = status
+
+    def __repr__(self):
+        return '<Task %r>' % self.task_name
+
+    def create_task(self):
+        record = Task(
+            task_name=self.task_name,
+            status=self.status
+        )
+        db.session.add(record)
+        db.session.commit()
+        return self
+
+
+class TaskSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Task
