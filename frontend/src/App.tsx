b7b81cb557faf9bf4afcbd20838a8c98f873a932
seus31 <us.balut.big.dipper@gmail.com>
2024-08-11 22:55:40 +0900

    fix: AppにAuthProvider,ApiProviderを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,33 +2,12 @@ import React from 'react'
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import './App.css'
-import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { getAccessToken, getStatus } from './libs/auth'
-import { setContext } from '@apollo/client/link/context'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { ApiProvider } from './providers/ApiContext'
+import { AuthProvider } from './providers/AuthContext'
+import DashboardContainer from './components/containers/DashboardContainer'
 import LoginContainer from './components/containers/LoginContainer'
-
-const authLink = setContext((_, { headers }) => {
-  const token = getAccessToken()
-  const status = getStatus()
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-      "X-STATUS": status,
-    },
-  }
-})
-
-const client = new ApolloClient({
-  link: authLink.concat(
-      new HttpLink({
-        uri: 'http://localhost:3000/graphql',
-      })
-  ),
-  cache: new InMemoryCache(),
-})
 
 const mdTheme = createTheme({
   palette: {
@@ -36,17 +15,22 @@ const mdTheme = createTheme({
   }
 })
 
+const apiUrl = 'http://localhost:5555/api/v1'
+
 const App: React.FC = () => {
   return (
       <ThemeProvider theme={mdTheme}>
         <BrowserRouter>
-          <ApolloProvider client={client}>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <Routes>
-                <Route path="/login" Component={LoginContainer} />
-              </Routes>
-            </LocalizationProvider>
-          </ApolloProvider>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <AuthProvider>
+              <ApiProvider apiUrl={apiUrl}>
+                <Routes>
+                  <Route path="/login" Component={LoginContainer} />
+                  <Route path="/dashboard" Component={DashboardContainer} />
+                </Routes>
+              </ApiProvider>
+            </AuthProvider>
+          </LocalizationProvider>
         </BrowserRouter>
       </ThemeProvider>
   )
