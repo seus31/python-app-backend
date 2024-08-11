db936927c687939e3ba1383ecc14f84e435590e5
seus31 <us.balut.big.dipper@gmail.com>
2024-08-11 22:42:58 +0900

    fix: jwtの設定追加及びCorsの設定修正

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+
 import router
 from config import config
 import db
@@ -11,19 +13,16 @@ def create_app():
 
     app.config.from_object(config.Config)
     db.init_db(app)
+    JWTManager(app)
     app.register_blueprint(router.router)
 
     app.config['JSON_AS_ASCII'] = False
     app.config["JSON_SORT_KEYS"] = False
-    CORS(
-        app,
-        resources={
-            r"/api/*": {"origins": ["http://localhost", "http://localhost:4200"]}
-        }
-    )
+    CORS(app)
+
     return app
 
 
 app = create_app()
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=8080, threaded=True, use_reloader=False)
+    app.run(host='0.0.0.0', debug=True, port=8080, threaded=True, use_reloader=True)
