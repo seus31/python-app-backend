252e3090a2167b92052833488e20e2d436696347
seus31 <us.balut.big.dipper@gmail.com>
2024-08-12 00:55:30 +0900

    存在しないpathをloginへリダイレクト

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { ApiProvider } from './providers/ApiContext'
-import { AuthProvider } from './providers/AuthContext'
+import { AuthProvider, useAuth } from './providers/AuthContext'
 import DashboardContainer from './components/containers/DashboardContainer'
 import LoginContainer from './components/containers/LoginContainer'
 import RegisterContainer from './components/containers/RegisterContainer'
@@ -20,21 +20,22 @@ const apiUrl = 'http://localhost:5555/api/v1'
 
 const App: React.FC = () => {
   return (
-      <ThemeProvider theme={mdTheme}>
-        <BrowserRouter>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <AuthProvider>
-              <ApiProvider apiUrl={apiUrl}>
-                <Routes>
-                  <Route path="/login" Component={LoginContainer} />
-                  <Route path="/register" Component={RegisterContainer} />
-                  <Route path="/dashboard" Component={DashboardContainer} />
-                </Routes>
-              </ApiProvider>
-            </AuthProvider>
-          </LocalizationProvider>
-        </BrowserRouter>
-      </ThemeProvider>
+    <ThemeProvider theme={mdTheme}>
+      <BrowserRouter>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <AuthProvider>
+            <ApiProvider apiUrl={apiUrl}>
+              <Routes>
+                <Route path="/login" Component={LoginContainer} />
+                <Route path="/register" Component={RegisterContainer} />
+                <Route path="/dashboard" Component={DashboardContainer} />
+                <Route path="*" element={<Navigate to="/login" />} />
+              </Routes>
+            </ApiProvider>
+          </AuthProvider>
+        </LocalizationProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
