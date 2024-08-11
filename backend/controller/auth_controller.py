31de57fed968e128a7894fd68e2c6423f4c08407
seus31 <us.balut.big.dipper@gmail.com>
2024-08-11 23:32:07 +0900

    add: ユーザー登録のコントローラーメソッド追加

--- backend/controller/auth_controller.py
+++ backend/controller/auth_controller.py
@@ -1,5 +1,10 @@
 from service.auth_service import login_logic
+from service.auth_service import register_logic
 
 
 def login():
     return login_logic()
+
+
+def register():
+    return register_logic()
