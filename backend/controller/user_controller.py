0a1a24c47009d5356fbbb7e45edfb0e11f86dec9
seus31 <us.balut.big.dipper@gmail.com>
2024-08-11 22:49:46 +0900

    fix: auth周りの追加による不要な実装を削除

--- backend/controller/user_controller.py
+++ backend/controller/user_controller.py
@@ -1,5 +1,4 @@
 from service.user_service import get_users_logic
-from service.user_service import post_user_logic
 from service.user_service import get_user_logic
 from service.user_service import update_user_logic
 from service.user_service import delete_user_logic
@@ -7,10 +6,6 @@ from service.user_service import delete_user_logic
 
 def get_users():
     return get_users_logic()
-
-
-def post_user():
-    return post_user_logic()
 
 
 def get_user(user_id):
--- backend/service/user_service.py
+++ backend/service/user_service.py
@@ -1,7 +1,6 @@
 from flask import make_response, request, jsonify
 from marshmallow import ValidationError
 from model.user import User, UserSchema
-from werkzeug.security import generate_password_hash
 
 
 def get_users_logic():
@@ -11,41 +10,6 @@ def get_users_logic():
         'code': 200,
         'users': user_schema.dump(users)
     }))
-
-
-def post_user_logic():
-    data = request.json
-    username = data.get('user_name')
-    email = data.get('email')
-    password = data.get('password')
-    password_confirm = data.get('password_confirm')
-
-    if not username or not email or not password or not password_confirm:
-        return make_response(
-            jsonify({
-                'code': 400,
-                'error': 'Missing required fields'
-            })
-        )
-
-    if User.query.filter_by(email=email).first():
-        return make_response(
-            jsonify({
-                'code': 400,
-                'error': 'Email already registered'
-            })
-        )
-
-    hashed_password = generate_password_hash(password)
-    new_user = User(name=username, email=email, password=hashed_password).create_user()
-    user_schema = UserSchema(many=False)
-
-    return make_response(
-        jsonify({
-            'code': 201,
-            'user': user_schema.dump(new_user)
-        })
-    )
 
 
 def get_user_logic(user_id):
