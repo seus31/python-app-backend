c7ab0e30737834e168987c219747cbbf36e30f5e
seus31 <us.balut.big.dipper@gmail.com>
2024-08-11 22:47:44 +0900

    add: ログイン用ルートの追加 fix: jwt認証を設定する

--- backend/router.py
+++ backend/router.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
+from flask_jwt_extended import jwt_required
 
+from controller import auth_controller
 from controller import task_controller
 from controller import user_controller
 from logging import config
 from json import load
-import auth
 import logger
 
 router = Blueprint('router', __name__)
@@ -13,51 +14,50 @@ with open("./config/logging.json", "r", encoding="utf-8") as f:
     config.dictConfig(load(f))
 
 
+@router.route("/api/v1/login", methods=['POST'])
+@logger.http_request_logging
+def api_v1_login():
+    return auth_controller.login()
+
+
 @router.route("/api/v1/users", methods=['GET'])
 @logger.http_request_logging
-@auth.requires_auth
+@jwt_required()
 def api_v1_users_get_user_list():
     return user_controller.get_users()
 
 
-@router.route("/api/v1/users", methods=['POST'])
-@logger.http_request_logging
-@auth.requires_auth
-def api_v1_users_post_user():
-    return user_controller.post_user()
-
-
 @router.route('/api/v1/users/<int:user_id>', methods=['GET'])
 @logger.http_request_logging
-@auth.requires_auth
+@jwt_required()
 def get_user(user_id):
     return user_controller.get_user(user_id)
 
 
 @router.route('/api/v1/users/<int:user_id>', methods=['PUT'])
 @logger.http_request_logging
-@auth.requires_auth
+@jwt_required()
 def update_user(user_id):
     return user_controller.update_user(user_id)
 
 
 @router.route('/api/v1/users/<int:user_id>', methods=['DELETE'])
 @logger.http_request_logging
-@auth.requires_auth
+@jwt_required()
 def delete_user(user_id):
     return user_controller.delete_user(user_id)
 
 
 @router.route("/api/v1/tasks", methods=['POST'])
 @logger.http_request_logging
-@auth.requires_auth
+@jwt_required()
 def api_v1_tasks_post_task():
     return task_controller.post_task()
 
 
 @router.route("/api/v1/tasks", methods=['GET'])
 @logger.http_request_logging
-@auth.requires_auth
+@jwt_required()
 def api_v1_tasks_gete_tasks():
     return task_controller.get_tasks()
 
