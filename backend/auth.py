3f9453a9afbaa28b5ab68e73a0cd2fd70a8b1a51
seus31 <us.balut.big.dipper@gmail.com>
2024-08-11 22:45:51 +0900

    delete: ファイルの削除

--- backend/auth.py
+++
@@ -1,9 +0,0 @@
-from flask import request
-from functools import wraps
-
-def requires_auth(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        auth = request.headers.get("Authorization", None)
-        return func(*args, **kwargs)
-    return wrapper
