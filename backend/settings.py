179a0bd316275738cefd149626596901fe3f7c85
seus31 <us.balut.big.dipper@gmail.com>
2024-08-11 22:47:03 +0900

    add: settingsにJWT_SECRET_KEYをenvから取得する

--- backend/settings.py
+++ backend/settings.py
@@ -7,6 +7,7 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 API_KEY = os.environ.get("API_KEY")
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
 MYSQL_ROOT_PASSWORD = os.environ.get("MYSQL_ROOT_PASSWORD")
 MYSQL_HOST = os.environ.get("MYSQL_HOST")
 MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE")
