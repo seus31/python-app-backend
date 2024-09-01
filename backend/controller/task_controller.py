96a09d955ebb95c66318959d1f958d52278b8697
seus31 <us.balut.big.dipper@gmail.com>
2024-09-01 23:48:46 +0900

    タスク更新APIのコントローラーメソッドの追加

--- backend/controller/task_controller.py
+++ backend/controller/task_controller.py
@@ -1,6 +1,7 @@
 from service.task_service import post_task_logic
 from service.task_service import get_tasks_logic
 from service.task_service import get_task_logic
+from service.task_service import update_task_logic
 from service.task_service import delete_task_logic
 
 
@@ -14,6 +15,10 @@ def get_tasks():
 
 def get_task(task_id):
     return get_task_logic(task_id)
+
+
+def update_task(task_id):
+    return update_task_logic(task_id)
 
 
 def delete_task(task_id):
